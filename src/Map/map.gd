7d768d2116ba75583d109723228e3ab2837d87dd
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2026-02-26 00:21:25 +0200

    Make fov radius adjustable rather than hardcoded

--- src/Map/map.gd
+++ src/Map/map.gd
@@ -28,7 +28,7 @@ func _place_tiles() -> void:
 		tiles.add_child(tile)
 
 func update_fov(player_position: Vector2i) -> void:
-	field_of_view.update_fov(map_data, player_position, 8)
+	field_of_view.update_fov(map_data, player_position, fov_radius)
 	
 	for entity in map_data.entities:
 		entity.visible = map_data.get_tile(entity.grid_position).is_in_view
